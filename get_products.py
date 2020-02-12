928400b48ad40151d37b7e899c1a10aff33ec8c5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-12 17:59:43 -0500

    Second challenge

--- get_products.py
+++ get_products.py
@@ -5,17 +5,21 @@ import json
 
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 
-request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products.json"
 print("URL:", request_url)
 
 response = requests.get(request_url)
 print(type(response))
 #print(dir(response))
-
 print(response.status_code)
-print(response.text)
+#print(response.text)
 print(type(response.text))
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-print(parsed_response["name"])
+print(type(parsed_response)) #> list
+
+for any_prod in parsed_response:
+    print(any_prod["id"], any_prod["name"])
+
+first_prod = parsed_response[0]
+print(first_prod["name"])
