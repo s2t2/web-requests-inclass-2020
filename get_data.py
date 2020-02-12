f72816476211810ead2663b8f71417bec6906492
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-12 17:41:57 -0500

    Get data

--- get_data.py
+++ get_data.py
@@ -1,3 +1,18 @@
 # get_data.py
 
+import requests
+
+
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+print("URL:", request_url)
+
+response = requests.get(request_url)
+print(type(response))
+#print(dir(response))
+
+print(response.status_code)
+print(response.text)
+
+breakpoint()
